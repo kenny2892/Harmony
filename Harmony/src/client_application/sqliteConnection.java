3412b7f514844e904e84f3744c493c6ade5293ba
Kenny <jessetuan@comcast.net>
2019-11-11 14:31:28 -0800

    Fix for Jar exporting

--- Harmony/src/client_application/sqliteConnection.java
+++ Harmony/src/client_application/sqliteConnection.java
@@ -1,4 +1,4 @@
-package application;
+package client_application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,7 +10,7 @@ public class sqliteConnection
 		try
 		{
 			Class.forName("org.sqlite.JDBC");
-			Connection connect = DriverManager.getConnection("jdbc:sqlite:" + sqliteConnection.class.getResource("/application/UserDatabase.sqlite"));
+			Connection connect = DriverManager.getConnection("jdbc:sqlite::resource:" + sqliteConnection.class.getResource("/client_application/UserDatabase.sqlite"));
 			
 			return connect;
 		}
